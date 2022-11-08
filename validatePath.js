747876b3c8dcc83692f5c9c573d55afcabd55afb
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2022-11-08 12:05:28 -0600

    Show somee code status.

--- validatePath.js
+++ validatePath.js
@@ -1,16 +1,35 @@
 const fs = require('fs');
 const process = require('node:process');
+const  fetch = require('node-fetch');
 
 const validatePath = (path) => {
     let result = 'no exist';
     try {
         if (fs.existsSync(path)){
             result =  'exist';
-            console.log(path[path.length-3], path[path.lenght-2], path[path.lenght-1]);
+            //aquí deberá validar sí es un archivo md
+            const rut = (path.split(".").pop());
+                if (rut == 'md') {
+                    fs.readFile(path, 'utf-8', (err, data) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            const links = Array.from(data.match(/\[(.+)\]\((https?:.+)\)/ig));
+                            links.forEach(texto => {
+                                const justLink = texto.split("(" ).pop().replace(")", "")
+                                fetch(justLink).then(response => {
+                                    console.log(response.status);
+                                })
+                            })
+                        }
+                    });
+                console.log('Si es un archivo md ');
         }
-    } catch(err) {
-        result = err;
-    }
-    return result;
+            else {console.log('No es un archivo MD');
+    }}
+} catch(err) {
+    result = err;
+}
+return result;
 }
 console.log(validatePath(process.argv[2]));
